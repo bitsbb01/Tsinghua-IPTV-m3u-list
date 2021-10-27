7d4714de53af916e0b4e05d827f80b486a64f521
bitsbb01 <61090794+bitsbb01@users.noreply.github.com>
2021-10-27 13:44:40 -0500

    Update m3u_convert.py

--- m3u_convert.py
+++ m3u_convert.py
@@ -14,7 +14,7 @@ f.write('#EXTM3U\n')
 for i in range(len(channels['Categories'])):
     for j in range(len(channels['Categories'][i]['Channels'])):
         line_1 = '#EXTINF:-1 group-title="' + channels['Categories'][i]['Name'] + '",' + channels['Categories'][i]['Channels'][j]['Name'] + '\n'
-        m3u8_url = 'https://raw.githubusercontent.com/bitsbb01/ez-iptvcat-scraper2/main/data/hls/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
+        m3u8_url = 'https://raw.githubusercontent.com/bitsbb01/IPTV-M3US/master/m3us/tvchans.m3u' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
         f.write(line_1)
         f.write(m3u8_url + '\n')
 
